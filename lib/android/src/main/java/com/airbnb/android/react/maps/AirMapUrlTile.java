3f0e02adba8a07836c30ba8ba14e6d96fcc9b999
Taavi Tilgre <taavi.tilgre@bitweb.ee>
2021-05-24 11:47:06 +0300

    uml tile suport for wms

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapUrlTile.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapUrlTile.java
@@ -11,42 +11,95 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AirMapUrlTile extends AirMapFeature {
+  private static final double[] mapBound = {-20037508.34789244, 20037508.34789244};
+  private static final double FULL = 20037508.34789244 * 2;
 
   class AIRMapUrlTileProvider extends UrlTileProvider {
     private String urlTemplate;
+    private int width;
+    private int height;
 
     public AIRMapUrlTileProvider(int width, int height, String urlTemplate) {
       super(width, height);
       this.urlTemplate = urlTemplate;
     }
 
+    private double convertY(int y, int zoom) {
+      double scale = Math.pow(2.0, zoom);
+      double n = Math.PI - (2.0 * Math.PI * y ) / scale;
+      return  Math.atan(Math.sinh(n)) * 180 / Math.PI;
+    }
+
+    private double[] getBoundingBox(int x, int y, int zoom) {
+      double scale = Math.pow(2.0, zoom);
+
+      double x1 = x/scale * 360 - 180;
+      double x2 = (x+1)/scale * 360 - 180;
+
+      double y1 = convertY(y+1,zoom);
+      double y2 = convertY(y,zoom);
+
+      return new double[]{
+              x1,
+              y1,
+              x2,
+              y2
+      };
+    }
+
     @Override
     public synchronized URL getTileUrl(int x, int y, int zoom) {
-
-      if (AirMapUrlTile.this.flipY == true) {
-        y = (1 << zoom) - y - 1;
+      Boolean isWMS = false;
+      if (this.urlTemplate.length() > 4) {
+        if (this.urlTemplate.substring(this.urlTemplate.length() - 5).equals("{WMS}")) {
+          isWMS = true;
+        }
       }
 
-      String s = this.urlTemplate
-          .replace("{x}", Integer.toString(x))
-          .replace("{y}", Integer.toString(y))
-          .replace("{z}", Integer.toString(zoom));
-      URL url = null;
+      if (isWMS) {
+        double[] bb = getBoundingBox(x, y, zoom);
+        String s = this.urlTemplate
+                .replace("{minX}", Double.toString(bb[0]))
+                .replace("{minY}", Double.toString(bb[1]))
+                .replace("{maxX}", Double.toString(bb[2]))
+                .replace("{maxY}", Double.toString(bb[3]))
+                .replace("{width}", Integer.toString(1024))
+                .replace("{height}", Integer.toString(1024))
+                .replace("{WMS}", "");
 
-      if(AirMapUrlTile.this.maximumZ > 0 && zoom > maximumZ) {
+        URL url = null;
+        try {
+          url = new URL(s);
+        } catch (MalformedURLException e) {
+          throw new AssertionError(e);
+        }
+        return url;
+      } else {
+        if (AirMapUrlTile.this.flipY) {
+          y = (1 << zoom) - y - 1;
+        }
+
+        String s = this.urlTemplate
+                .replace("{x}", Integer.toString(x))
+                .replace("{y}", Integer.toString(y))
+                .replace("{z}", Integer.toString(zoom));
+        URL url = null;
+
+        if (AirMapUrlTile.this.maximumZ > 0 && zoom > maximumZ) {
+          return url;
+        }
+
+        if (AirMapUrlTile.this.minimumZ > 0 && zoom < minimumZ) {
+          return url;
+        }
+
+        try {
+          url = new URL(s);
+        } catch (MalformedURLException e) {
+          throw new AssertionError(e);
+        }
         return url;
       }
-
-      if(AirMapUrlTile.this.minimumZ > 0 && zoom < minimumZ) {
-        return url;
-      }
-
-      try {
-        url = new URL(s);
-      } catch (MalformedURLException e) {
-        throw new AssertionError(e);
-      }
-      return url;
     }
 
     public void setUrlTemplate(String urlTemplate) {
